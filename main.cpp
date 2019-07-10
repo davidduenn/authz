ed5251f78d6a178702a40af189c3f70498701230
David Duenn <davidduenn@gmail.com>
2019-07-09 20:31:20 -0400

    Check permissions

--- main.cpp
+++ main.cpp
@@ -26,6 +26,7 @@ class user_node{
     user_node(int id, string name);
     void add_type_node(string type);
     void add_perm_node(string type, string perm);
+    int has_perm(string type, string perm);
   private:
     int id;
     string name;
@@ -38,6 +39,10 @@ int main(){
   user_node users(123, "david");
   users.add_type_node("article");
   users.add_perm_node("article","editor");
+  cout << "has" << (users.has_perm("article","editor")?"":" no") << " permission" << endl;
+  cout << "has" << (users.has_perm("article","publisher")?"":" no") << " permission" << endl;
+  cout << "has" << (users.has_perm("news","editor")?"":" no") << " permission" << endl;
+  cout << "has" << (users.has_perm("news","publisher")?"":" no") << " permission" << endl;
 }
 
 
@@ -55,4 +60,12 @@ void user_node::add_perm_node(string type, string perm){
   // need to search for and use only this type
   this->types->perms = new perm_node;
   this->types->perms->perm = perm;
+}
+
+int user_node::has_perm(string type, string perm){
+  if(this->types->type == type &&
+     this->types->perms->perm == perm){
+    return 1;
+  }
+  return 0;
 }
